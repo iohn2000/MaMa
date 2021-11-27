cd85b507f00d31fe0c3ce1c3634fe62f14edfb9b
Johannes Fleck <FleckJ@kapsch.net>
2021-11-27 01:11:44 +0100

    test class to draw pdf

--- SyncFusionPdfTest/Program.cs
+++ SyncFusionPdfTest/Program.cs
@@ -13,16 +13,55 @@ namespace SyncFusionPdfTest
     {
         static void Main(string[] args)
         {
-            //Create a new PDF document.
-            PdfDocument doc = new PdfDocument();
-            doc.PageSettings.Size = PdfPageSize.A4;
-            PdfMargins marg = new PdfMargins(); marg.All = 15f;
-            doc.PageSettings.Margins = marg;
+            // Draw a header : https://www.syncfusion.com/kb/9789/how-to-draw-multiline-text-in-pdf-using-c-and-vb-net
+            //Draw PdfLightTable.
+            //PdfGraphics graphics = page.Graphics;
+            //PdfFont font2 = new PdfStandardFont(PdfFontFamily.Helvetica, 14);
 
+            RechenAufgabenPdf p = new RechenAufgabenPdf();
+            p.SavePdf();
+        }
+
+
+    }
+
+    public class RechenAufgabenPdf
+    {
+        PdfDocument doc = new PdfDocument();
+        
+
+        public void SavePdf()
+        {
+            doc = DrawPdf();
+
+            //Save the document.
+            var fileStream = File.Create("out.pdf");
+            doc.Save(fileStream);
+            doc.Close(true);
+        }
+        public PdfDocument DrawPdf()
+        {
+            //Create a new PDF document.
+            
+            PdfMargins marg = new PdfMargins();
+            marg.All = 15f;
+            doc.PageSettings.Size = PdfPageSize.A4;
+            doc.PageSettings.Margins = marg;
             PdfPage page = doc.Pages.Add();
 
-            PdfLightTable pdfLightTable = new PdfLightTable();
+            var pdf = CreateTable();
+            var data = FillData();
+            //Assign data source.
+            pdf.DataSource = data;
 
+            pdf.Draw(page, new PointF(0, 0));
+
+
+            return doc;
+        }
+
+        public DataTable FillData()
+        {
             // Initialize DataTable to assign as DateSource to the light table.
             DataTable table = new DataTable();
             //Include columns to the DataTable.
@@ -32,33 +71,30 @@ namespace SyncFusionPdfTest
 
             //Include rows to the DataTable.
 
-            table.Rows.Add(new string[] { "399   / 0,6  = 665",     "59,52 / 0,16 = 372", "142   x 9,67 = 1373,14" });
-            table.Rows.Add(new string[] { "822,6 / 0,18 = 4570",    "810,9 / 0,05 = 16218" });
-            table.Rows.Add(new string[] { "4392  / 0,09 = 48800",   "28,9  x 132  = 3814,8" });
-            table.Rows.Add(new string[] { "0,907 x 580  = 526,060", "0,609 x 798  = 485,982" });
+            for (int i = 0; i < 55; i++)
+            {
+                table.Rows.Add(new string[] { "399   / 0,6  = 665", "59,52 / 0,16 = 372", "142   x 9,67 = 1373,14" });
+                table.Rows.Add(new string[] { "822,6 / 0,18 = 4570", "810,9 / 0,05 = 16218" });
+                table.Rows.Add(new string[] { "4392  / 0,09 = 48800", "28,9  x 132  = 3814,8" });
+                table.Rows.Add(new string[] { "0,907 x 580  = 526,060", "0,609 x 798  = 485,982" });
+            }
+            return table;
+        }
 
+        public PdfLightTable CreateTable()
+        {
 
-            //Assign data source.
-            pdfLightTable.DataSource = table;
+            PdfLightTable pdfLightTable = new PdfLightTable();
+            PdfCellStyle myStyle = new PdfCellStyle();
 
-            //Draw PdfLightTable.
-
-            PdfGraphics graphics = page.Graphics;
             PdfFont font1 = new PdfStandardFont(PdfFontFamily.Courier, 12);
-            //PdfFont font2 = new PdfStandardFont(PdfFontFamily.Helvetica, 14);
-            PdfCellStyle myStyle = new PdfCellStyle(); 
             myStyle.Font = font1;
-            pdfLightTable.Style.DefaultStyle = myStyle;
             myStyle.BorderPen = new PdfPen(Color.Transparent);
+
+            pdfLightTable.Style.DefaultStyle = myStyle;
+
             pdfLightTable.BeginRowLayout += PdfLightTable_BeginRowLayout;
-            pdfLightTable.Draw(page, new PointF(0, 0));
-
-
-            //Save the document.
-            var fileStream = File.Create("out.pdf");
-            doc.Save(fileStream);
-
-            doc.Close(true);
+            return pdfLightTable;
         }
 
         private static void PdfLightTable_BeginRowLayout(object sender, BeginRowLayoutEventArgs args)
